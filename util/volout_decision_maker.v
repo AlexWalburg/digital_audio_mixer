665ce0367f932e54d23d2f073c10f2ef778c7ea6
Alex Walburg <ajwalburg@gmail.com>
2024-01-31 19:35:21 -0800

    Change parameters to match top level mapping, remove timed reg

--- util/volout_decision_maker.v
+++ util/volout_decision_maker.v
@@ -1,24 +1,30 @@
 module volout_decision_maker
-  #(parameter VOL_BITS = 23,
+  #(parameter VOL_BITS = 24,
     parameter CPC_BITS = 15,
     parameter NUM_DECIMAL = 8)
-  (input signed [VOL_BITS-1:0] vol,
-   input			cpc_en,
-   input [CPC_BITS-1:0]		cpc,
-   output signed [VOL_BITS-1:0]	vol_out);
-
-   always #* begin
-      if (cpc_en)begin
-	 if(CPC >= 2 << NUM_DECIMAL)
-	   vol_out = vol >>> 1;
-	 else if(CPC <= 1 << (NUM_DECIMAL-1))
-	   vol_out = vol << 1;
-	 else if(CPC <= 1 << (NUM_DECIMAL-2))
-	   vol_out = vol << 2;
-	 else
-	   vol_out <= vol;
-      end
-      else
-	vol_out <= vol;
-   end
+  (
+    input			 clk,
+    input			 rst,
+    input signed [VOL_BITS-1:0]	 vol,
+    input			 cpc_en,
+    input [CPC_BITS-1:0]	 cpc,
+    output signed [VOL_BITS-1:0] vol_out);
+    
+    reg [CPC_BITS-1:0] cpc_int;
+    
+    assign vol_out = 
+        (cpc_int >= 1 << (NUM_DECIMAL + 2)) ? vol >>> 1 :
+        (cpc_int >= 1 << (NUM_DECIMAL - 1)) ? vol << 1 :
+        (cpc_int >= 1 << (NUM_DECIMAL - 2)) ? vol << 2 :
+        vol;
+    
+    always @(posedge clk or posedge rst) begin
+       if (rst) begin
+          cpc_int <= 0;
+       end
+       else begin
+	  if(cpc_en)
+            cpc_int <= cpc;
+       end
+    end
 endmodule // volout_decision_maker
